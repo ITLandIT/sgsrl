53c4875faa06e2c53cba3fd2b084304518f748e7
ITLandIT <luigizotti@itland.it>
2024-06-25 16:56:15 +0200

    a

--- addons/time_slide_monitor/__manifest__.py
+++ addons/time_slide_monitor/__manifest__.py
@@ -1,16 +1,11 @@
 {
     'name': 'Slide Time Tracking',
-    'version': '1.0',
+    'version': '1.1',  # Aggiornato
     'author': 'ITLand',
     'depends': ['base', 'web', 'website_slides'],
     'data': [
         'views/slide_template.xml',
     ],
-    'assets': {
-        'web.assets_backend': [
-            'static/src/js/slide_timer.js',
-        ],
-    },
     'installable': True,
     'application': True,
 }
