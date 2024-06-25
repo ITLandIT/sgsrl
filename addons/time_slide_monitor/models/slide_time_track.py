601ef5cd64946776a8c24f3df175d3feba44df90
ITLandIT <luigizotti@itland.it>
2024-06-25 15:51:00 +0200

    try to fix 2

--- addons/time_slide_monitor/models/slide_time_track.py
+++ addons/time_slide_monitor/models/slide_time_track.py
@@ -1,8 +1,5 @@
 # models/slide_time_track.py
-from odoo import models, fields, api
-import logging
-
-_logger = logging.getLogger(__name__)
+from odoo import models, fields
 
 class SlideTimeTrack(models.Model):
     _name = 'slide.time.track'
@@ -11,9 +8,3 @@ class SlideTimeTrack(models.Model):
     user_id = fields.Many2one('res.users', string='User', required=True)
     slide_id = fields.Many2one('slide.slide', string='Slide', required=True)
     time_spent = fields.Float(string='Time Spent (seconds)', default=0.0)
-    start_time = fields.Datetime(string='Start Time', default=fields.Datetime.now)
-
-    @api.model
-    def create(self, values):
-        _logger.info('Creating new slide time track entry: %s', values)
-        return super(SlideTimeTrack, self).create(values)
