fb5cafb80147049689ec6b7981a69dbc11ecd3de
ITLandIT <luigizotti@itland.it>
2024-06-25 09:43:18 +0200

    Install module check certificate

---
+++ addons/website_tutorial/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Website Tutorial',
+    'description': 'A simple website tutorial module',
+    'author': 'Your Name',
+    'version': '2.9',
+    'depends': ['website'],
+    'data': [
+        'views/templates.xml',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'application': True
+}
