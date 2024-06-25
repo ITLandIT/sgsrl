5532f3be51c1c1bc8dc546fc8a23894b7b02e87c
ITLandIT <luigizotti@itland.it>
2024-06-25 16:43:48 +0200

    a

--- addons/time_slide_monitor/models/slide_time_track.py
+++ addons/time_slide_monitor/models/slide_time_track.py
@@ -1,10 +1,9 @@
-# models/slide_time_track.py
+# slide_time_track.py
 from odoo import models, fields
 
 class SlideTimeTrack(models.Model):
     _name = 'slide.time.track'
     _description = 'Slide Time Tracking'
 
-    user_id = fields.Many2one('res.users', string='User', required=True)
-    slide_id = fields.Many2one('slide.slide', string='Slide', required=True)
-    time_spent = fields.Float(string='Time Spent (seconds)', default=0.0)
+    # your fields here
+    name = fields.Char(string='Name', required=True)
