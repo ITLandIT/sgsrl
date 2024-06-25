40df80a6e663746bfc6cfa54052efec2d3a0d0e8
ITLandIT <luigizotti@itland.it>
2024-06-25 11:47:40 +0200

    install module check certificated

---
+++ addons/website_tutorial/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Verifica certificazioni',
+    'description': 'Modulo per la verifica dei certificati rilasciati',
+    'author': 'ITLand',
+    'version': '2.10',
+    'depends': ['website'],
+    'data': [
+        'views/templates.xml',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'application': True
+}
